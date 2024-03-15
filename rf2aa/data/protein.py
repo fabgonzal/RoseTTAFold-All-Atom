0be0aa9d46baf0e36f3a86f02db22a769bfad02e
fgonzal <129413756+fabgonzal@users.noreply.github.com>
2024-03-15 10:55:15 -0400

    Update protein.py

--- rf2aa/data/protein.py
+++ rf2aa/data/protein.py
@@ -90,4 +90,4 @@ def load_protein(msa_file, hhr_fn, atab_fn, model_runner):
 
 def generate_msa_and_load_protein(fasta_file, chain, model_runner):
     msa_file, hhr_file, atab_file = make_msa(fasta_file, chain, model_runner)
-    return load_protein(str(msa_file), str(hhr_file), str(atab_file), model_runner)
+    return load_protein(msa_file, hhr_file, atab_file, model_runner)
